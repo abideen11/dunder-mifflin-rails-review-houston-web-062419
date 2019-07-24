6636cfbd2b2b3e23ed469227e66b88fbb3028d32
abideen11 <abideenkareem17@gmail.com>
2019-07-24 16:15:46 -0500

    learn

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,27 @@
 class DogsController < ApplicationController
+    # before_action :current_dog, only: [:show, :edit, :update]
+    def index
+        @dogs = Dog.all
+    end
+    def new
+        @dog = Dog.new
+    end
+    def show
+        @dog = Dog.find(params[:id])
+    end
+    
+
+    def create
+        @dog = Dog.create(dogs_params)
+    end
+    def edit 
+        @dog = Dog.find(params[:id])
+    end
+    private 
+    def dogs_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
+    def current_dog
+        @dog = Dog.find(params[:id])
+    end 
 end
